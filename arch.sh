2c51d1f495793cfd453fa155f1f5efc7ff6045fd
yurhett <46419702+yurhett@users.noreply.github.com>
2024-06-14 10:47:34 +0800

    Update arch.sh

--- arch.sh
+++ arch.sh
@@ -1,8 +1,17 @@
 #!/bin/sh
-if [ "$TARGETARCH" = "amd64" ] ; then ARCH=linux;fi
+if [ "$TARGETARCH" = "amd64" ] ; then ARCH=amd64;fi
 if [ "$TARGETARCH" = "arm64" ] ; then ARCH=aarch64;fi
 if [ "$TARGETARCH" = "arm" ] ; then ARCH=arm;fi
-TAG=$1
-VER=$2
-echo "https://github.com/hpool-dev/chiapp-miner/releases/download/${TAG}/HPool-Miner-chia-pp-${VER}-${ARCH}.zip "
-wget -q --no-check-certificate https://github.com/hpool-dev/chiapp-miner/releases/download/${TAG}/HPool-Miner-chia-pp-${VER}-${ARCH}.zip -O /tmp/chia-miner.zip && unzip -j /tmp/chia-miner.zip -d /tmp/linux
+
+VER=$1
+BUILD_VER=$2
+echo ${VER}
+echo ${BUILD_VER}
+ZIP_URL="https://github.com/h9-dev/chiapp-miner/releases/download/v${VER}/H9-Miner-chia-pp-v${VER}-${BUILD_VER}-linux.zip"
+
+
+echo ${ZIP_URL}
+wget -q --no-check-certificate ${ZIP_URL} -O /tmp/chia-miner.zip && unzip -j /tmp/chia-miner.zip -d /tmp/linux
+ls /tmp/linux -ahl
+mv /tmp/linux/h9-miner-chia-linux-${ARCH} /opt/h9-chia-miner
+chmod +x /opt/h9-chia-miner
